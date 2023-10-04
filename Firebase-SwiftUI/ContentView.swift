2b8fa4426eae1ff95dd4f98dc747dd1184e2bd82
Alexey Kolch <kolychenkov94@mail.ru>
2023-10-04 14:41:19 +0400

    Design + Firebase add
    Firebase without dependency with app.

--- Firebase-SwiftUI/ContentView.swift
+++ Firebase-SwiftUI/ContentView.swift
@@ -6,18 +6,110 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        SignIn()
     }
 }
+
+struct SignIn : View {
+
+    @State var user = ""
+    @State var pass = ""
+    @State var message = ""
+    @State var alert = false
+    @State var show = false
+
+    var body : some View{
+        VStack {
+             Text("Sign In").fontWeight(.heavy).font(.largeTitle).padding([.top,.bottom], 20)
+            VStack{
+
+                VStack(alignment: .leading){
+
+                    VStack(alignment: .leading){
+
+                        Text("Username").font(.headline).fontWeight(.light).foregroundColor(Color.init(.label).opacity(0.75))
+
+                        HStack{
+
+                            TextField("Enter Your Username", text: $user)
+
+                            if user != ""{
+
+                                Image("check").foregroundColor(Color.init(.label))
+                            }
+
+                        }
+
+                        Divider()
+
+                    }.padding(.bottom, 15)
+
+                    VStack(alignment: .leading){
+
+                        Text("Password").font(.headline).fontWeight(.light).foregroundColor(Color.init(.label).opacity(0.75))
+
+                        SecureField("Enter Your Password", text: $pass)
+
+                        Divider()
+                    }
+
+                }.padding(.horizontal, 6)
+
+            }.padding()
+///Кнопка Sign In
+            VStack{
+                Button(action: {
+                    //Code
+                }) {
+                    Text("Sign In").foregroundColor(.white).frame(width: UIScreen.main.bounds.width - 120).padding()
+                }.background(Color(.green))
+                    .clipShape(Capsule())
+                    .padding(.top, 45)
+
+                Text("(or)").foregroundColor(Color.gray.opacity(0.5)).padding(.top,30)
+
+                HStack(spacing: 8) {
+                    Text("Don't Have An Account ?").foregroundColor(Color.gray.opacity(0.5))
+///Кнопка Sign Up
+                    Button(action: {
+                        //Code
+                    }) {
+                        Text("Sign Up")
+                    }.foregroundColor(.blue)
+
+                }.padding(.top, 25)
+            }
+        }
+    }
+}
+///Вход в приложение
+func signInWithEmail(email: String, password: String, completion: @escaping (Bool, String) -> Void) {
+    Auth.auth().signIn(withEmail: email, password: password) { res, err in
+        if err != nil {
+            completion(false, (err?.localizedDescription)!)
+            return
+        }
+
+        completion(true, (res?.user.email)!)
+    }
+}
+///Регистрация в приложение
+func signUpWithEmail(email: String, password: String, completion: @escaping (Bool, String) -> Void) {
+    Auth.auth().createUser(withEmail: email, password: password) { res, err in
+        if err != nil {
+            completion(false, (err?.localizedDescription)!)
+            return
+        }
+
+        completion(true, (res?.user.email)!)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Firebase-SwiftUI/Firebase_SwiftUIApp.swift
+++ Firebase-SwiftUI/Firebase_SwiftUIApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
 
 @main
 struct Firebase_SwiftUIApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
