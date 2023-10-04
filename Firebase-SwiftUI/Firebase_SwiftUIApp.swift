140bf9ffe4e25a1cd8ab56e508bdfb709e2535ac
Alexey Kolch <kolychenkov94@mail.ru>
2023-10-04 13:23:49 +0400

    Initial Commit

---
+++ Firebase-SwiftUI/Firebase_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_SwiftUIApp.swift
+//  Firebase-SwiftUI
+//
+//  Created by Алексей Колыченков on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
